456e93a87cf27f67e8a7abf41169c5cf77b3788c
Brian <bbensch@gmail.com>
2017-10-13 00:07:49 -0700

    set flash conversion for lead firing

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -87,6 +87,7 @@ class InstructorsController < ApplicationController
         # ga_test_cid = params[:ga_client_id]
         # puts "The GA ga_client_id is #{ga_test_cid}."
         session[:instructor_id] = @instructor.id
+        flash[:application_submitted] = 'TRUE'
         GoogleAnalyticsApi.new.event('instructor-recruitment', 'new-application-submitted', params[:ga_client_id])
         format.html { redirect_to '/thanks-for-applying', notice: 'Your instructor application was successfully submitted, you will be contacted shortly. You may also reach out with questions to info@snowschoolers.com' }
         format.json { render action: 'show', status: :created, location: @instructor }
