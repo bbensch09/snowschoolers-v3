49f9006b1cad86f7f687ba1239e241ffc380a5d1
Brian <bbensch@gmail.com>
2017-12-28 01:53:34 -0800

    remove faulty time filter for next day cutoff at 430pm -- TEMP

--- app/assets/javascripts/lessons.js
+++ app/assets/javascripts/lessons.js
@@ -2,7 +2,7 @@
 var dt = new Date();
 var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 if (time > "16:00:00") {
-    var MIN_DATE = 2
+    var MIN_DATE = 1
   } else {
     var MIN_DATE = 1
   };
