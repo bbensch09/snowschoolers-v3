ce034dd6e007b9d2eeaf2b1e3d85237d6a24dcad
Brian <bbensch@gmail.com>
2018-01-01 19:46:47 -0800

    fix booked state

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -518,7 +518,7 @@ class Lesson < ActiveRecord::Base
   end
 
   def eligible_for_payroll?
-    eligible_states = ['finalizing','finalizing payment & reviews','Payment complete, waiting for review.','waiting for payment','Lesson Complete','confirmed']
+    eligible_states = ['finalizing','finalizing payment & reviews','Payment complete','waiting for review.','waiting for payment','Lesson Complete','confirmed']
     #removed 'confirmed' from active states to avoid sending duplicate SMS messages.
     eligible_states.include?(state) && self.this_season?
   end
