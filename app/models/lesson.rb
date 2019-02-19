87b89a02c37b63839adca64bfc580e53016ef669
bbensch09 <bbensch@gmail.com>
2019-02-19 07:29:32 -0800

    update payroll to include all booked lessons and pending states, format dates w/ year

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -714,7 +714,7 @@ class Lesson < ActiveRecord::Base
   end
 
   def eligible_for_payroll?
-    eligible_states = ['finalizing','finalizing payment & reviews','Payment complete','waiting for review','waiting for payment','Lesson Complete','confirmed']
+    eligible_states = ['finalizing','finalizing payment & reviews','Payment complete','waiting for review','waiting for payment','Lesson Complete','confirmed','booked','pending instructor','seeking replacement instructor']
     #removed 'confirmed' from active states to avoid sending duplicate SMS messages.
     eligible_states.include?(state) && self.this_season?
   end
