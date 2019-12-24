42eddbf7402e7bd513eea31ec5e4aad229599217
Brian Bensch <bbensch@gmail.com>
2019-12-23 21:17:53 -0800

    save progress working on skills_practiced

--- app/models/report_card.rb
+++ app/models/report_card.rb
@@ -1,4 +1,6 @@
 class ReportCard < ApplicationRecord
+  	has_and_belongs_to_many :skills_practiced
+  	has_and_belongs_to_many :skills_recommended
 	belongs_to :student
 	belongs_to :instructor
 	belongs_to :lesson
