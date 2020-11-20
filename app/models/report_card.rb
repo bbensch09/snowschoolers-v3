411d180c5ea087a2e5cbcb08faa593cd38d49ea2
Brian Bensch <bbensch@gmail.com>
2020-11-20 08:37:39 -0800

    disallow modal registrations >> add recaptcha to users/sign_up

--- app/models/report_card.rb
+++ app/models/report_card.rb
@@ -1,5 +1,4 @@
-class ReportCard < ApplicationRecord
-  	has_and_belongs_to_many :skills_practiced
+class ReportCard < ApplicationRecord has_and_belongs_to_many :skills_practiced
   	has_and_belongs_to_many :skills_recommended
 	belongs_to :student
 	belongs_to :instructor
