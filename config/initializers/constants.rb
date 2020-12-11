4bbba2c9c5cf0304e5fe5a4fd77ea88c7cdddc45
Brian Bensch <bbensch@gmail.com>
2020-12-11 02:13:23 -0500

    remove airbnb slots, fix tyupos in release

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -30,13 +30,13 @@ KV_HOLIDAYS = ['2020-12-20','2020-12-21','2020-12-22','2020-12-23','2020-12-24',
 SLEDDING_SLOTS = ['Early-bird (9-10:30am)', 'Morning (11am-12:30pm)','Midday (1-2:30pm)','Closing(3-4:30pm)']
 
 PRIVATE_SLOTS = [
-	'1hr Early Bird 8:45-9:45am','1hr Private 10:00am','1hr Private 11:15am','1hr Private 12:30pm','Half-day Morning 10:00am-12:45pm','Half-day Afternoon 1:15-4:00pm','2hr Group Afternoon 1:45pm-3:45pm','Full-day (10:00am-4:00pm)','Airbnb Morning 10:00am-12:00pm','Airbnb Afternoon 1:30-3:30pm'
+	'1hr Early Bird 8:45-9:45am','1hr Private 10:00am','1hr Private 11:15am','1hr Private 12:30pm','Half-day Morning 10:00am-12:45pm','Half-day Afternoon 1:15-4:00pm','Full-day (10:00am-4:00pm)'
 ]
 
 GROUP_SLOTS = ['2hr Group Afternoon 1:45pm-3:45pm','Airbnb Morning 10:00am-12:00pm','Airbnb Afternoon 1:30-3:30pm']
 
 ADMIN_SLOTS = [
-	 'Airbnb Morning 10:00am-12:00pm','Airbnb Afternoon 1:30-3:30pm','1hr Early Bird 8:45-9:45am','1hr Private 10:00am','1hr Private 11:15am','1hr Private 12:30pm','1hr Private 1:45pm','1hr Private 3:00pm','Half-day Morning 10:00am-12:45pm','Half-day Afternoon 1:15-4:00pm','2hr Group Afternoon 1:45pm-3:45pm','Full-day (10:00am-4:00pm)'
+	 'Airbnb Morning 10:00am-12:00pm','Airbnb Afternoon 1:30-3:30pm','1hr Early Bird 8:45-9:45am','1hr Private 10:00am','1hr Private 11:15am','1hr Private 12:30pm','1hr Private 1:45pm','1hr Private 3:00pm','Half-day Morning 10:00am-12:45pm','Half-day Afternoon 1:15-4:00pm','Full-day (10:00am-4:00pm)'
 ]
 
 VALID_GROUP_SLOTS = ['2hr Group Afternoon 1:45pm-3:45pm']
